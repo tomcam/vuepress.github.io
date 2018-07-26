979772e98ed78aa241c5580ed6c83e07e237e22d
tomcam <tomcampbell@gmail.com>
2018-07-26 04:25:21 -0700

    Minimal sidebar test upgrade

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,6 +2,9 @@ module.exports = {
     title: "VuePress Book",
 	themeConfig: {
 		sidebar: [
+			'/',
+			['./default1.md', 'Creating a website'],
+			['./netlify.md', 'Deploying to Netlify']
 		]
 	}
 }
