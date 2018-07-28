768c6cbe1f19dead47e6bcec243d1cdcf32a8daf
tomcam <tomcampbell@gmail.com>
2018-07-27 20:21:37 -0700

    Implement sidebar

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,10 +1,37 @@
 module.exports = {
     title: "VuePress Book",
 	themeConfig: {
+		
 		sidebar: [
-			'/',
-			['./default1.md', 'Creating a website'],
-			['./netlify.md', 'Deploying to Netlify']
+			{
+				title: 'Tutorial',
+				children: [
+					'/default1.md', 
+					'/default2.md',
+					'/default3.md',
+					'/default4.md',
+					'/default5.md',
+					'/default6.md',
+					'/default7.md',
+					'/default8.md',
+					'/config1.md',
+					'/config-title.md',
+					'/config-description.md',
+				]
+			},
+			'/style-overriding-default.md',
+			['/netlify.md', 'Deploying to Netlify'],
+			{
+				title: 'Custom themes',
+				children: [
+					'/custom1.md',
+					'/custom2.md',
+				]
+			},
+			'/cheatsheet.md',
+			'/credits.md',
+			
 		]
 	}
 }
+
