379dbe00f57b08d03aec032c08e21726beb37924
Tom Campbell <tomcampbell@gmail.com>
2018-08-18 15:07:38 -0700

    Update config.js

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,6 +2,13 @@ module.exports = {
 	title: 'VuePress tutorial',
 	description: 'VuePress examples and demo code',
 	ga: 'UA-124245419-1',
+	serviceWorker: true,
+	head: [
+		['link', { rel: 'icon', href: '/hero.png' }],
+		['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    		['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+		['link', { rel: 'apple-touch-icon', href: `/icons/apple-touch-icon-152x152.png` }],
+	],
 	themeConfig: {
 		sidebar: [
 			'/introduction',
